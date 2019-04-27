9e5d89a0b44ef37bbea4a6fb7bab5a966e540252
Kkelly22 <kk504408@gmail.com>
2019-04-27 13:01:19 -0400

    fixed syntax bug to say PLANS instead of ALL

--- client/src/reducers/managePlans.js
+++ client/src/reducers/managePlans.js
@@ -2,12 +2,12 @@ export default function managePlans(state = {plans: []}, action) {
 	switch (action.type) {
 	    case 'FETCH_PLANS':
 	    debugger
-	      return { ...state, all: action.payload }
+	      return { ...state, plans: action.payload }
 	    case 'CREATE_PLAN':
 	    debugger
-	      return { ...state, all: [...state.all, action.payload] }
+	      return { ...state, plans: [...state.plans, action.payload] }
 	    case 'DELETE_PLAN':
-	      return { ...state, all: state.all.filter(plan => plan.id !== action.payload.id) }
+	      return { ...state, plans: state.plans.filter(plan => plan.id !== action.payload.id) }
 	    default: return state
 	}
 }
