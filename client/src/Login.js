9c00690c32c22345674b046f5668db81dbb07f64
Kkelly22 <kk504408@gmail.com>
2019-04-27 21:50:25 -0400

    added a Login and Signup render file

---
+++ client/src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import LoginContainer from './containers/LoginContainer';
+
+function Login() {
+  return (
+    <div className="App">
+      < LoginContainer />
+    </div>
+  );
+}
+
+export default Login;
---
+++ client/src/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SignupContainer from './containers/SignupContainer';
+
+function Signup() {
+  return (
+    <div className="App">
+      < SignupContainer />
+    </div>
+  );
+}
+
+export default Signup;
