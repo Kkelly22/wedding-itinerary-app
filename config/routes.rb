8c4a837d8f74ea9bfb912628a1b8a91bad24ac9f
Kkelly22 <kk504408@gmail.com>
2019-04-27 21:56:17 -0400

    added custom login and signup routes that talk with the useres controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   resources :plans
   resources :users
+
+   get 'user', to: 'users#show', as: 'user_show'
+   post 'signup', to: 'users#create', as: 'user_signup'
+   post 'login', to: 'users#login', as: 'user_login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
