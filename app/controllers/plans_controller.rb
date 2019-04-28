43628d188a5901c481df880884eab9e3a90d9875
Kkelly22 <kk504408@gmail.com>
2019-04-27 21:54:27 -0400

    added a current user to the plans controller

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,45 +1,19 @@
 class PlansController < ApplicationController
 	def index
-		@plans = current_user.plans.all
-		respond_to do |format|
-      		format.html { render :index }
-      		format.json { render json: @plans }
-    	end
+		@plans = get_current_user.plans.all
+		render json: @plans 
     end
 
-	def new
-		@plan = plan.new
-	end 
-
 	def create
-		@plan = plan.create(plan_params)
+		@plan = get_current_user.plans.build(plan_params)
+    	@plan.save
 		render json: @plan, status: 201
-	end
-
-	def show
-		@plan = plan.find_by(id: params[:id])
-		respond_to do |format|
-      		format.html { render :show }
-      		format.json { render json: @plan }
-    	end
-	end
-
-	def edit
-		@plan = plan.find_by(id: params[:id])
-	end
-
-	def update
-		@plan = plan.find_by(id: params[:id])
-		@plan.update(plan_params)
-		respond_to do |format|
-      		format.html { render :show }
-      		format.json { render json: @plan }
-    	end
 	end
 
 	def destroy
 		@plan = plan.find_by(id: params[:id])
 		@plan.destroy
+		render json: @plan, status: 201
 	end
 
 	private
