833010ee0005949ef18a178d0375f559c430bb8b
Kkelly22 <kk504408@gmail.com>
2019-04-30 15:08:14 -0400

    added Wedding Countdown container logic and styling

---
+++ client/src/components/plans/WeddingCountdown.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './../../App.css';
+import Countdown from 'react-countdown-now';
+
+class WeddingCountdown extends Component {
+  render() {
+    return (
+      <div class="countdown-info">
+        Time Until Wedding: <Countdown date={'Sat, 16 Nov 2019 05:00:00'} />
+      </div>
+    );
+  }
+};
+
+export default WeddingCountdown;
+
+
+
--- client/src/containers/PlansContainer.js
+++ client/src/containers/PlansContainer.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux'
 import PlanInput from '../components/plans/PlanInput'
 import Plans from '../components/plans/Plans'
 import UserInfo from '../components/users/UserInfo'
+import WeddingCountdown from '../components/plans/WeddingCountdown'
 import { fetchPlans, deletePlan } from '../actions/planActions'
 
 class PlansContainer extends Component {
@@ -19,6 +20,7 @@ class PlansContainer extends Component {
         <UserInfo />
         <Plans plans={this.props.plans} deletePlan={this.props.deletePlan} />
         <PlanInput />
+        <WeddingCountdown />
       </div>
     )
   }
