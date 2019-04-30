e5e09b67eeb1347a9213da4e2bff32d5f2d61206
Kkelly22 <kk504408@gmail.com>
2019-04-29 21:24:34 -0400

    removed debuggers

--- client/src/reducers/managePlans.js
+++ client/src/reducers/managePlans.js
@@ -1,10 +1,8 @@
 export default function managePlans(state = {plans: []}, action) {
 	switch (action.type) {
 	    case 'FETCH_PLANS':
-	    debugger
 	      return { ...state, plans: action.payload }
 	    case 'CREATE_PLAN':
-	    debugger
 	      return { ...state, plans: [...state.plans, action.payload] }
 	    case 'DELETE_PLAN':
 	      return { ...state, plans: state.plans.filter(plan => plan.id !== action.payload.id) }
