021be9eafe67729b2b0f91ab39273a4361f2afc3
Kkelly22 <kk504408@gmail.com>
2019-04-29 21:27:11 -0400

    moved React Router from index and into App

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import PlansContainer from './containers/PlansContainer';
+import Signup from './Signup';
+import Login from './Login';
+
+import { BrowserRouter as Router, Route, Redirect, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      < PlansContainer />
+      <Router> 
+  		<Switch id="routes">
+      	<Route exact path='/' component={PlansContainer} />
+      	<Route exact path='/plans' component={PlansContainer} />
+	      <Route exact path='/signup' render={Signup} />
+	      <Route exact path='/login' render={Login} />
+	      <Route exact path='/logout' render={Login} />
+    	</Switch>
+  	  </Router>
     </div>
   );
 }
 
+
 export default App;
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,36 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import managePlans from './reducers/managePlans';
-
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware, compose} from 'redux';
+import managePlans from './reducers/managePlans';
 
 import './index.css';
 import App from './App';
-import Signup from './Signup';
-import Login from './Login';
 
 const store = createStore(managePlans, compose(applyMiddleware(thunk),window.devToolsExtension ? window.devToolsExtension() : f => f))
 
 ReactDOM.render((
   <Provider store={store}>
-  	<Router>
-  		<React.Fragment>
-      	<Route exact path="/" render={App} />
-      	<Route path="/plans" render={App} />
-      	<Route path="/signup" render={Signup} />
-      	<Route path="/login" render={Login} />
-    	</React.Fragment>
-  	</Router>
+  	<App/>
   </Provider>),
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
