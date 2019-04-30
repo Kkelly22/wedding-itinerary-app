5c5fe725574e85b2a3e05bef3ccbc4601b59ff62
Kkelly22 <kk504408@gmail.com>
2019-04-30 13:31:13 -0400

    formatted itinerary display into a table

--- client/src/components/plans/Plan.js
+++ client/src/components/plans/Plan.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './../../App.css';
 
 class Plan extends Component {
 
@@ -6,11 +7,13 @@ class Plan extends Component {
     const { plan } = this.props;
 
     return (
-      <div>
-        <li>
-          {plan.description}
-          <button onClick={() => this.props.deletePlan(plan.id)}> X </button>
-        </li>
+      <div class="plan-data">
+        <td>{plan.description}</td>
+        <td>{plan.vendor}</td>
+        <td>{plan.location}</td>
+        <td>{plan.time}</td>
+        <td>{plan.completed}</td>
+        <td><button onClick={() => this.props.deletePlan(plan.id)}> X </button></td>
       </div>
     );
   }
--- client/src/components/plans/Plans.js
+++ client/src/components/plans/Plans.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import Plan from './Plan'
-
+import './../../App.css';
 
 class Plans extends Component {
   render(){
 
   	const plans = this.props.plans.map(plan => <Plan key={plan.id} plan={plan} deletePlan={this.props.deletePlan}/>)
-
   return (
-    <div>
-      {plans}
+    <div class="plan-header">
+     <table>
+     	<thead>
+          <tr>
+            <th>Description</th>
+            <th>Vendor</th>
+            <th>Location</th>
+            <th>Time</th>
+            <th>Completed?</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+          	{plans}
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
  };
 };
 
 export default Plans;
+
