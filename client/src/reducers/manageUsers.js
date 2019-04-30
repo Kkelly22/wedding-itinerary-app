cfead52100d76369d325baee4bf6fbfa99af2f23
Kkelly22 <kk504408@gmail.com>
2019-04-29 21:25:03 -0400

    added current key to the state

--- client/src/reducers/manageUsers.js
+++ client/src/reducers/manageUsers.js
@@ -1,7 +1,7 @@
 export default function manageUsers(state = {}, action) {
 	switch (action.type) {
 		case 'SET_USER':
-	      return { ...state, action.payload }
+	      return { ...state, current: action.payload }
 	    default: return state
 	}
 }
