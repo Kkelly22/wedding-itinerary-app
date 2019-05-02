d5f49eb930f00c740932a9638df6135fa4ddd2c7
Kkelly22 <kk504408@gmail.com>
2019-05-02 19:09:08 -0400

    changed login and logout from POST to GET requests

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
    get 'user', to: 'users#show', as: 'user_show'
    post 'signup', to: 'users#create', as: 'user_signup'
-   post 'login', to: 'users#login', as: 'user_login'
-   post 'logout', to: 'users#logout', as: 'user_logout'
+   get 'login', to: 'users#login', as: 'user_login'
+   get 'logout', to: 'users#logout', as: 'user_logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
