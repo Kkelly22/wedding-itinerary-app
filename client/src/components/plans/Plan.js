052bf37377cc7f488e548f3cdcd31ab1301a928f
Kkelly22 <kk504408@gmail.com>
2019-06-09 22:49:26 -0400

    added voting button to test out a component not tied to store

--- client/src/components/plans/Plan.js
+++ client/src/components/plans/Plan.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import './../../App.css';
 import Moment from 'react-moment';
 
+import VoteCount from './VoteCount.js';
+
 class Plan extends Component {
+  state = {
+    count: 0
+  }
+
+  handleOnSubmit() {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
 
   render() {
     const { plan } = this.props;
+
     return (
       <div className="plan-data">
         <td>{plan.description}</td>
@@ -14,6 +26,8 @@ class Plan extends Component {
         <td> <Moment parse="YYYY-MM-DD HH:mm">{plan.time}</Moment></td>
         <td>{ plan.completed ? "Yes" : "No" }</td>
         <td><button onClick={() => this.props.deletePlan(plan)}> X </button></td>
+        <td><button onClick={() => this.handleOnSubmit()}> V </button></td>
+        <VoteCount counter={this.state.count} />
       </div>
     );
   }
