e8d3b11ab0a86ea1e2bab98e2057128505e2c064
Kkelly22 <kk504408@gmail.com>
2019-08-11 12:33:47 -0400

    used figaro to add gmail environment variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
