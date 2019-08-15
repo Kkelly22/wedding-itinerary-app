43dee8209a1d772a8011e30d8154f6d5c08be3d3
Kkelly22 <kk504408@gmail.com>
2019-08-15 08:25:17 -0400

    renamed the email view file

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ApplicationMailer
 
 	def welcome_email
     	@user = params[:user]
-    	mail(to: @user.email, subject: 'Welcome to Your Wedding Itinerary')
+    	mail(to: 'kk504408@gmail.com', subject: 'Welcome to Your Wedding Itinerary')
+    #	mail(to: @user.email, subject: 'Welcome to Your Wedding Itinerary')
   	end
 end
