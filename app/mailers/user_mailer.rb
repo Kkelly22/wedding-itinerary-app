7b4ced69cf5d257c8811dcbfb803a37cd97ce0d9
Kkelly22 <kk504408@gmail.com>
2019-08-15 21:03:41 -0400

    reactivated user email property now that it exists

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,6 @@ class UserMailer < ApplicationMailer
 
 	def welcome_email
     	@user = params[:user]
-    	mail(to: 'kk504408@gmail.com', subject: 'Welcome to Your Wedding Itinerary')
-    #	mail(to: @user.email, subject: 'Welcome to Your Wedding Itinerary')
+    	mail(to: @user.email, subject: 'Welcome to Your Wedding Itinerary')
   	end
 end
