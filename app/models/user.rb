3614738ba9b29729d907e7e407c523067b310236
Kkelly22 <kk504408@gmail.com>
2019-08-25 10:43:01 -0400

    added validations to enforce the presence of all user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
 	has_secure_password
  	validates :username, presence: true, uniqueness: true
  	validates :password, presence: true
-
+ 	validates :bride, :groom, :wedding_location, :wedding_date, :email, presence: true
 end
